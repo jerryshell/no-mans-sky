f276c00d1e85a7479b397babb67086c6229d32bf
Jerry <calljerryli@outlook.com>
2021-03-20 00:16:27 +0800

    feat: 主要逻辑完成

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,57 @@ use std::time::Duration;
 use chrono::Utc;
 
 fn main() {
+    let mut pid: u32 = 0;
     loop {
         thread::sleep(Duration::from_secs(1));
         let user_count = get_user_count();
         println!("{} user_count: {}", Utc::now(), user_count);
+        if user_count > 0 && pid != 0 {
+            clean_env();
+            kill_target_process(&mut pid);
+        }
+        if user_count <= 0 && pid == 0 {
+            init_env();
+            pid = start_target_process();
+        }
     }
 }
 
 fn get_user_count() -> usize {
     let w_output = Command::new("w").arg("-h").output().unwrap().stdout;
     String::from_utf8_lossy(&w_output).lines().count()
+}
+
+fn init_env() {
+    println!("init_env()");
+    let mut process = Command::new("bash");
+    process.arg("init.sh");
+    process.spawn().unwrap().wait();
+}
+
+fn clean_env() {
+    println!("clean_env()");
+    let mut process = Command::new("bash");
+    process.arg("clean.sh");
+    process.spawn().unwrap();
+}
+
+fn start_target_process() -> u32 {
+    println!("start_target_process()");
+    let mut process = Command::new("bash");
+    process.arg("start.sh");
+    let child = process.spawn().unwrap();
+    let pid = child.id();
+    println!("target process pid: {}", pid);
+    return pid;
+}
+
+fn kill_target_process(pid: &mut u32) {
+    println!("kill_target_process()");
+    let mut process = Command::new("kill");
+    process.arg("-9");
+    process.arg(pid.to_string());
+    // let output = process.output().unwrap().stdout;
+    // println!("output {}", String::from_utf8_lossy(&output));
+    *pid = 0;
 }

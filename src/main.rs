7255e16235e1759a7645423ec1200a2dd07d0dc2
Jerry <calljerryli@outlook.com>
2021-04-24 10:38:01 +0800

    chore: simplify code

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use std::{
 use no_mans_sky::*;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    let kill_at = args.get(1);
+    let kill_at = env::args().nth(1);
     let task = Arc::new(Mutex::new(eth_task::ETHTask));
     match kill_at {
         Some(kill_at_str) => run(task, Some(kill_at_str.parse::<u64>().unwrap())),
